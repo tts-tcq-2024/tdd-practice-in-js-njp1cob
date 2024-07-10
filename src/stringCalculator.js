660fa7c3a8872281d5f40a46274cdb14763c568a
njp1cob <p.anjana@in.bosch.com>
2024-07-10 18:38:21 +0530

    Update stringCalculator.js

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,11 +1,11 @@
 
 
-function add(value) {
+function add(number) {
   if (number == "" || number == "0") {
     return 0;
     } else {
      let sum = 0;
-    let count = value.split(",");
+    let count = number.split(",");
     for (let i = 0; i < count.length; i++) {
       sum = sum + parseInt(count[i]);
    }
