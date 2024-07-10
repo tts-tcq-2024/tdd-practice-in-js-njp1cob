c968a7aa4cacc2931626b2071a9bc1dea0611055
njp1cob <p.anjana@in.bosch.com>
2024-07-10 18:17:43 +0530

    Update stringCalculator.js

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,14 +1,23 @@
 
+let sum = 0;
 function add(value) {
-    if (value == "" || value == "0") {
+ let count = value.split(",");
+ for (let i = 0; i < count.length; i++) {
+   sum = sum + parseInt(count[i]);
+ }
+ return sum;
+}
+
+function checkValue(number) {
+   if (value == "" || value == "0") {
     return 0;
     } else {
-    return 1;
-    }
+      add(); 
+    }  
 }
 
 
-
 module.exports = {
-    add
+    add,
+    checkValue
 };
